5c9e3345318c7df5759fc606562c8efaf67a5416
vamsi <vamsi11287@gmail.com>
2023-05-27 08:29:02 +0530

    first_Commit

--- server/DB/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.set("strictQuery",true)
-mongoose.connect('mongodb://127.0.0.1/admin')
-.then(()=>console.log("db connected"))
-.catch((err) =>{
-    console.log(err)
-})
